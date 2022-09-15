a09ec6b00ec58b247658c757fa4e9bbfa83f25a1
ethan <ethan.gonzalez00774@gmail.com>
2022-09-15 13:09:18 -0700

    first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -12,4 +12,16 @@ describe('helloWorld', function() {
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
+
+
+
+
+describe("say hello",function()
+{
+            it('should ', function ()
+            {
+                expect(typeof helloWorld).toBe("function");
+            } );
+}       )
+
 });
