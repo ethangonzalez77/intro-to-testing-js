6ef578ade50fd53cdb1d082436f83382089d5cb2
ethan <ethan.gonzalez00774@gmail.com>
2022-09-16 08:35:45 -0700

    q7 complete

--- code.js
+++ code.js
@@ -2,3 +2,17 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(input) {
+    if(typeof input === "string" && input.length > 0){
+        return "Hello, " + input + "!";
+    }
+    else{
+        return "Hello, World!";
+    }
+
+}
+
+
+
+
--- tests.js
+++ tests.js
@@ -12,16 +12,75 @@ describe('helloWorld', function() {
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
+})
+// sayHello() function unit tests
+describe('sayHello', ()=> {
+    it('the sayHello() function should be defined', () => {
+        expect(typeof sayHello).toBe('function')
+    })
+
+    it('should return a string when called', () => {
+        expect(typeof sayHello()).toBe('string')
+    })
+
+    it('should return Hello Ethan! when called with sayHello("Ethan")', () => {
+        expect(sayHello('Ethan')).toBe('Hello, Ethan!')
+    })
+    it('should return Hello Pat! when called with sayHello("Alex")', () => {
+        expect(sayHello('Alex')).toBe('Hello, Alex!')
+    })
+
+    it('should return Hello Pat! when called with sayHello("Pat")', () => {
+        expect(sayHello('Pat')).toBe('Hello, Pat!')
+    })
+
+    it('should return the string "Hello, World!" when called', () => {
+        expect(sayHello()).toBe('Hello, World!')
+    })
+    it("should never return 'undefined' when called", function() {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+})
+
+
+// describe('newStuff', function (){
+//     it("should have 'newStuff' defined as a function",function(){
+//         expect(typeof newstuff).toBe('function');
+//     });
+// })
 
 
 
 
-describe("say hello",function()
-{
-            it('should ', function ()
-            {
-                expect(typeof helloWorld).toBe("function");
-            } );
-}       )
+    // it('should return a string when called', function() {
+    //     expect(helloWorld()).toBe("string");
+    // });
 
-});
+
+
+
+
+// describe("say hello",function()
+// {
+//             it('should ', function ()
+//             {
+//                 expect(typeof helloWorld).toBe("function");
+//             } );
+// }       )
+
+
+
+
+// describe("say hello", function()
+//     {
+//             it('should return a string when called',function ()
+//             {
+//                 expect(typeof sayhello().toBe("string"))
+//
+//             } );
+//             it('say hello'), function (){
+//                 expect(typeof sayhello().toBe("string"))
+//             }
+//     }   )
+//
+//
